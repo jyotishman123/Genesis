8c79948f4e4f6e3c8f1726d47ef37876282f038e
Jyotishman Goswami <jyotishman2468@gmail.com>
2024-01-30 02:39:53 +0530

    Api-working

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,35 @@
-import Image from "next/image";
+import { gql, request } from "graphql-request";
 
-export default function Home() {
+export default async function Home() {
+  const GET_BLOGS = gql`
+  query Publication {
+    publication(host: "webcentric.hashnode.dev") {
+        isTeam
+         title
+     about{
+        markdown
+     }
+   }
+ }
+  `;
+
+  const endpoint = 'https://gql.hashnode.com';
+  const apiKey = '47c9830b-9375-4476-9703-d0e0eb604856';
+
+  const headers = {
+    Authorization: `Bearer ${apiKey}`,
+  };
+
+  try {
+    const data = await request(endpoint, GET_BLOGS, headers);
+    console.log(data);
+  } catch (error) {
+    console.error('Error fetching blogs:', error);
+  }
+
   return (
     <main>
-         <h1>Hashnode graphqlApi</h1>
+      <h1>Hashnode graphqlApi</h1>
     </main>
   );
 }
