e31f87626b5d59de441bf59c164dc88603357ec2
Devkant <devkant21@gmail.com>
2024-01-31 18:35:45 +0530

    Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
- import HeroSection from "@/components/HeroSection"
-export default  function Home() {
+import HeroSection from "@/components/HeroSection"
+import { getPost } from '@/helper/getPost';
+import { getUserInfo } from "@/helper/getUserInfo";
 
-
+export default async function Home() {
+   const userDetails = await getUserInfo();
+   const data = await getPost();
+   if (!data) {
+      return;
+   }
+   const { username, profilePicture, name } = data.user;
    return(
       <>  
-        <div>Home Page</div>
+         <div className="p-5 text-2xl">Hi this is {userDetails?.me?.name}. I am a {userDetails?.me?.bio?.markdown}</div>
       </>  
         
    )
